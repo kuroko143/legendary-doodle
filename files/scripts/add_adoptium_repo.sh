c5f8048e1e5907536bd9f75fae208bc3721d57da
Kuroko143 <jchan.utoronto@gmail.com>
2025-12-16 23:17:59 -0500

    Fixed path

--- files/scripts/add_adoptium_repo.sh
+++ files/scripts/add_adoptium_repo.sh
@@ -8,9 +8,10 @@ set -oue pipefail
 cat <<EOF > /etc/yum.repos.d/adoptium.repo
 [Adoptium]
 name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/\$releasever/\$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+enabled_metadata=1
 EOF
 
