47952ac5d0a49986e29d020a83375f534ccd878e
Kuroko143 <jchan.utoronto@gmail.com>
2025-12-20 16:01:33 -0500

    KDE Rounded Corners

---
+++ files/scripts/build_kde_rounded_corners.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+mkdir -p /tmp/build/rounded
+cd /tmp/build/rounded
+
+git clone https://github.com/matinlotfali/KDE-Rounded-Corners
+cd KDE-Rounded-Corners
+mkdir build
+cd build
+cmake ..
+cmake --build . -j
+make install
+
+cd /
+rm -rf /tmp/build/rounded
